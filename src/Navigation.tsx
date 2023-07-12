db69bac2344e8459607bcdd8818d957f6f2e866f
thseogns <thseo1758@naver.com>
2023-07-12 16:35:02 +0900

    make some topButton

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -2,20 +2,34 @@
 import { Link } from "react-router-dom";
 import TopButton from "./button/TopButton";
 import styles from "./Navigation.module.css";
+import { useState } from "react";
 
 const Navigation: React.FC = () => {
   const buttonName = ["Home", "Info", "Skill"];
-
+  const [toggleButton, setToggleButton] = useState("Home");
+  const handleToggle = (buttonName: string) => {
+    console.log(buttonName);
+    setToggleButton(buttonName);
+  };
   return (
     <div className={styles.flex}>
-      <Link to="/Home">
-        <TopButton name={buttonName[0]} />
+      <Link to="/Home" onClick={() => handleToggle(buttonName[0])}>
+        <TopButton
+          name={buttonName[0]}
+          toggleButton={toggleButton === buttonName[0]}
+        />
       </Link>
-      <Link to="/Info">
-        <TopButton name={buttonName[1]} />
+      <Link to="/Info" onClick={() => handleToggle(buttonName[1])}>
+        <TopButton
+          name={buttonName[1]}
+          toggleButton={toggleButton === buttonName[1]}
+        />
       </Link>
-      <Link to="/Skill">
-        <TopButton name={buttonName[2]} />
+      <Link to="/Skill" onClick={() => handleToggle(buttonName[2])}>
+        <TopButton
+          name={buttonName[2]}
+          toggleButton={toggleButton === buttonName[2]}
+        />
       </Link>
     </div>
   );
--- src/button/TopButton.tsx
+++ src/button/TopButton.tsx
@@ -4,9 +4,17 @@ import React from "react";
 import styles from "./TopButton.module.css";
 interface TopButtonProps {
   name: string;
+  toggleButton?: boolean;
 }
-const TopButton = ({ name }: TopButtonProps) => {
-  return <div className={styles.boxStyle}>{name}</div>;
+const TopButton = ({ name, toggleButton }: TopButtonProps) => {
+  return (
+    <div
+      className={styles.boxStyle}
+      style={toggleButton ? { opacity: 1 } : { opacity: 0.7 }}
+    >
+      {name}
+    </div>
+  );
 };
 
 export default TopButton;
