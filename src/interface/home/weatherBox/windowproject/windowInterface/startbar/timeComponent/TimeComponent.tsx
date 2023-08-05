0bc91288613272ce4c6503fc47309585730296e0
thseogns <thseo1758@naver.com>
2023-08-06 06:17:37 +0900

    create TimeComponent

--- src/interface/home/weatherBox/windowproject/windowInterface/startbar/timeComponent/TimeComponent.tsx
+++ src/interface/home/weatherBox/windowproject/windowInterface/startbar/timeComponent/TimeComponent.tsx
@@ -1,9 +1,64 @@
 /** @format */
 
-import React from "react";
-
+import React, { useState } from "react";
+import styles from "./TimeComponent.module.css";
 const TimeComponent = () => {
-  return <div>TimeComponent</div>;
+  const [time, setTime] = useState("");
+  const [todayDate, setTodayDate] = useState("");
+  const [hourOfTime, setHourOfTime] = useState(0);
+
+  let stringMonth = "";
+  let stringDate = "";
+  let stringMinutes = "";
+  setInterval(() => {
+    clock();
+  }, 1000);
+
+  function clock() {
+    let today = new Date();
+
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1; // 월
+    let date = today.getDate(); // 날짜
+    let hours = today.getHours(); // 시
+    let minutes = today.getMinutes(); // 분
+    stringMinutes = minutes.toString();
+    //시간
+    if (month < 10 && month.toString().length < 3) {
+      stringMonth = "0" + month.toString();
+    }
+
+    if (date < 10 && date.toString().length < 3) {
+      stringDate = "0" + date.toString();
+    }
+    if (minutes < 10 && minutes.toString().length < 3) {
+      stringMinutes = "0" + minutes.toString();
+      console.log(stringMinutes);
+    }
+    if (hours > 12) {
+      hours = hours - 12;
+    }
+
+    setHourOfTime(hours);
+
+    // if (hours > 11) {
+    //   document.getElementById("mf").innerHTML = "오후";
+    // } else {
+    //   document.getElementById("mf").innerHTML = "오전";
+    // }
+
+    setTime(hours + ":" + stringMinutes);
+    setTodayDate(year + "-" + stringMonth + "-" + stringDate);
+  }
+  return (
+    <div className={styles.todayCover}>
+      <div className={styles.timeCover}>
+        {hourOfTime > 11 ? <span>오후</span> : <span>오전</span>}
+        {time}
+      </div>
+      <div className={styles.dateCover}>{todayDate}</div>
+    </div>
+  );
 };
 
 export default TimeComponent;
